c0e4832d66523fdd3647f72f6c38528327f99de5
MarceloAndreola <marceloandreola18@gmail.com>
2025-09-16 16:54:15 -0300

    Cloudinary prueba 6

--- cloudinary_config.py
+++ cloudinary_config.py
@@ -4,10 +4,13 @@ import cloudinary.api
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# Solo cargamos el .env si existe (para desarrollo local)
+if os.path.exists(".env"):
+    load_dotenv()
 
 cloudinary.config(
-    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key = os.getenv("CLOUDINARY_API_KEY"),
-    api_secret = os.getenv("CLOUDINARY_API_SECRET")
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True  # asegura que la URL que te devuelve sea HTTPS
 )
