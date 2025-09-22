253eaa0e13cf17760baaf01ef5791ab43a721f67
MarceloAndreola <marceloandreola18@gmail.com>
2025-09-22 16:54:45 -0300

    Pasarela de pago con mercado pago 3

--- app/routes/payment.py
+++ app/routes/payment.py
@@ -1,13 +1,21 @@
 import os
 import mercadopago
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, make_response
 
 payment_bp = Blueprint("payment", __name__)
 
 sdk = mercadopago.SDK(os.getenv("MP_ACCESS_TOKEN"))
 
-@payment_bp.route('/create_preference', methods=['POST'])
+@payment_bp.route('/create_preference', methods=['POST', 'OPTIONS'])
 def create_preference():
+    if request.method == 'OPTIONS':
+        # Respuesta para preflight
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
+        return response
+
     data = request.json
     preference_data = {
         "items": [
@@ -18,16 +26,15 @@ def create_preference():
             }
         ],
         'back_urls': {
-            'success': 'https://ecomerce-plantilla-back-1.onrender.com/success',
-            'failure': 'https://ecomerce-plantilla-back-1.onrender.com/failure',
-            'pending': 'https://ecomerce-plantilla-back-1.onrender.com/pending',
+            'success': 'https://e-commerce-plantilla-frontend.netlify.app/success',
+            'failure': 'https://e-commerce-plantilla-frontend.netlify.app/failure',
+            'pending': 'https://e-commerce-plantilla-frontend.netlify.app/pending',
         },
         'auto_return': 'approved',
         'payment_methods': {
             'installments': 12
         }
     }
-
 
     preference_response = sdk.preference().create(preference_data)
     return jsonify(preference_response['response'])
