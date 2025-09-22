2463f0e6a48886aa36d07e37371ca346ff109994
MarceloAndreola <marceloandreola18@gmail.com>
2025-09-22 16:48:34 -0300

    Pasarela de pago con mercado pago 2

--- app/routes/payment.py
+++ app/routes/payment.py
@@ -6,7 +6,7 @@ payment_bp = Blueprint("payment", __name__)
 
 sdk = mercadopago.SDK(os.getenv("MP_ACCESS_TOKEN"))
 
-@payment_bp.route('create_preference', methods=['POST'])
+@payment_bp.route('/create_preference', methods=['POST'])
 def create_preference():
     data = request.json
     preference_data = {
@@ -18,16 +18,16 @@ def create_preference():
             }
         ],
         'back_urls': {
-            'success': 'http://localhost:8080/success',
-            'failure': 'http://localhost:8080/failure',
-            'pending': 'http://localhost:8080/pending',
+            'success': 'https://ecomerce-plantilla-back-1.onrender.com/success',
+            'failure': 'https://ecomerce-plantilla-back-1.onrender.com/failure',
+            'pending': 'https://ecomerce-plantilla-back-1.onrender.com/pending',
         },
         'auto_return': 'approved',
         'payment_methods': {
             'installments': 12
         }
     }
-    
+
 
     preference_response = sdk.preference().create(preference_data)
     return jsonify(preference_response['response'])
