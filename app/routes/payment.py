e9ab109ef5197fa8fdb2065268ec5dfbc943c8b9
MarceloAndreola <marceloandreola18@gmail.com>
2025-09-23 15:40:45 -0300

    Pasarela de pago con mercado pago 8

--- app/routes/payment.py
+++ app/routes/payment.py
@@ -11,6 +11,7 @@ def create_preference():
         return jsonify({'message': 'Preflight OK'}), 200
     try:
         data = request.json
+        print("Data recibida:", data)
         preference_data = {
             "items": [
                 {
@@ -29,12 +30,8 @@ def create_preference():
                 'installments': 12
             }
         }
-
         preference_response = sdk.preference().create(preference_data)
         return jsonify(preference_response['response'])
-
     except Exception as e:
-        import traceback
-        print("🔥 Error en create_preference:", e)
-        traceback.print_exc()
+        print("Error en create_preference:", e)
         return jsonify({'error': str(e)}), 500
